10e99b543097dd1a30197e03a360bddfd841b5d9
wangke <1668505874@qq.com>
2016-08-11 15:34:40 +0800

    start

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,11 +25,8 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
 // 注册路由...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
-//Route::post('auth/register', 'Auth\AuthController@postRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
 
-
-//sadasdsadsad
-//asdas
 
 Route::get('/articles','ArticlesController@index');
 Route::get('/articles/{id}','ArticlesController@detail');
