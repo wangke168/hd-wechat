1a4170756aa845db348288dc1443d802b6f1edde
wangke <1668505874@qq.com>
2016-08-12 14:56:53 +0800

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,7 +23,7 @@ Route::get('about',function(){
 Route::get('/articles','ArticlesController@index');
 Route::get('/articles/{id}','ArticlesController@detail');
 
-//asda
+//asdaaa
 
 /*Laravel-Wechat*/
 Route::any('/wechat', 'WechatController@serve');
