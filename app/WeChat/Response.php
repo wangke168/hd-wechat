1e4a629d4a6cd7f851e1e01848cb542347a3808d
wangke <1668505874@qq.com>
2016-08-19 13:58:53 +0800

    edit text request

--- app/WeChat/Response.php
+++ app/WeChat/Response.php
@@ -49,15 +49,15 @@ class Response
                 break;
             default:
 
-//                $row=DB::table('wx_article')->where('title','like','门票%')->orderBy('id','desc')->skip(0)->take(1)->get();
+                $row=DB::table('wx_article')->where('title','like','门票%')->orderBy('id','desc')->skip(0)->take(1)->get();
 
-//                foreach ($row as $result) {
+                foreach ($row as $result) {
                     $news1 = new News();
-                    $news1->title = "laravel-wechat";
+                    $news1->title = $result->title;
                     $news1->description = "测试";
                     $news1->url = "http://www.baidu.com";
                     $news1->image = "http://www.hengdianworld.com/images/JQ/scenic_dy.png";
-//                }
+                }
                 return [$news1];
                 break;
         }
