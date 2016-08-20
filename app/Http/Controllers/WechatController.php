c14975baf8fb5c382117b177fe5fff52cce7404a
wangke <1668505874@qq.com>
2016-08-20 14:16:12 +0800

    edit text request

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -26,14 +26,14 @@ class WechatController extends Controller
                     switch ($message->Event) {
                         case 'CLICK':
                             switch ($message->EventKey) {
-                                case "2":
-                                    $content = $response->click_request("2");
-                                    return $content;
-                                    break;
                                 case "13":
                                     $content = new Text();
                                     $content->content = "横店影视城官方客服电话" . "\n" . "400-9999141";
                                     return $content;
+                                default:
+                                    $content = $response->click_request("2");
+                                    return $content;
+                                    break;
                             }
                             break;
                         case 'subscribe':
