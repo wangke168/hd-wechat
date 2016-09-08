ad2aa16aedc8b5013c963532acfe95f64281b490
wangke168 <wangke168@gmail.com>
2016-09-08 00:44:19 -0700

    auto send show info

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\Inspire::class,
+        \App\Console\Commands\Inspire::class,
         \App\Console\Commands\Update_Openid_Info::class,
         \App\Console\Commands\auto_send_show_info::class,
     ];
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('inspire')
-            ->cron('*/20 8-18 * * *');
+            ->everyMinute();
         //更新wx_user_info里的信息
         $schedule->command('update_openid_info')
             ->dailyAt('2:00');
