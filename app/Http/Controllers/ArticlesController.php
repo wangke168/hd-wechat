d119fa99ec140eb42141a042c341c544961e04a6
wangke168 <wangke168@gmail.com>
2016-09-09 14:49:04 +0800

    auto cancel tag

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -48,9 +48,10 @@ class ArticlesController extends Controller
 //        $openIds = ['opUv9v1jQ2jTF4AIxirvBg2jrr_c', 'opUv9v977Njll_YHpZYMymxI_aPE'];
 //        return $tag->batchTagUsers($openIds, $tagId);
         $openIds = $tag->usersOfTag($tagId, $nextOpenId = '')->data;
-        return $openIds;
+        return $tag->batchUntagUsers($openIds, $tagId);
 
-//        return $tag->batchUntagUsers($openIds, $tagId);
+        $openIds = $tag->usersOfTag($tagId, $nextOpenId = '')->data;
+        return $openIds;
 
 //        return $tags;
     }
