3b09dccceb94566a144d4e5a0d14b9d04cee2728
wangke168 <wangke168@gmail.com>
2016-09-09 16:53:52 +0800

    auto remind ldjl

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -56,7 +56,7 @@ class ArticlesController extends Controller
         $row=DB::table('tour_project_wait_detail')
             ->where('project_id',$project_id)
             ->where('used',0)
-            ->whereDay('addtime','=',date('Y-m-d'))
+//            ->whereDay('addtime','=',date('Y-m-d'))
             ->where('verification_time','>',Carbon::now())
             ->whereRaw('UNIX_TIMESTAMP(verification_time)<=' . $endtime)
             ->get();
