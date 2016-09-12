265c29ec1337ab2f3bb9e442f4926531104c60e8
wangke168 <wangke168@gmail.com>
2016-09-12 15:02:11 +0800

    fix auto send show info

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -40,11 +40,53 @@ class ArticlesController extends Controller
 
     public function info()
     {
-        $usage=new Usage();
-        $qrscene_id = $usage->get_eventkey_son_info('91');
-        if ($qrscene_id) {
-            foreach ($qrscene_id as $eventkey) {
-                echo $eventkey;
+        $app=app('wechat');
+        $row=DB::table('wx_location_list')
+            ->orderBy('id','desc')
+            ->get();
+        foreach ($row as $result) {
+            $aaa = explode(',', $result->show_time);
+            $prevtime = date('Y-m-d');
+            foreach ($aaa as $bbb) {
+                $temptime = (strtotime($bbb) - strtotime("now")) / 60;
+                if ($temptime < 30 && $temptime > 0) {
+
+                    $row1 = DB::table('wx_user_info')
+                        ->where('eventkey', $result->eventkey)
+                        ->where('scandate', date('Y-m-d'))
+                        ->whereRaw('UNIX_TIMESTAMP(endtime)>=' . strtotime($prevtime))
+                        ->get();
+
+                    foreach ($row1 as $send_openid) {
+                        $content = new Text();
+                        $content->content = "您好，" . $result->zone_id . "景区" . $result->show_name . "的演出时间是" . $bbb . "。还没到剧场的话要抓紧了哦。\n如果您不知道剧场位置，<a href='" . $result->location_url . "'>点我</a>\n微信演出时间有时无法及时更新，以景区公示为准。";
+                        $app->staff->message($content)->by('1001@u_hengdian')->to($send_openid->wx_openid)->send();
+                    }
+
+
+                    /*检查景区eventkey下有没有其他二维码，例：龙帝惊临项目在秦王宫里，因此龙帝惊临和秦王宫的二维码是从属关系，扫龙帝惊临的二维码也能收到秦王宫的节目提醒*/
+//                    $qrscene_id=$this->get_eventkey_info($result['eventkey']);
+                    $Usage = new Usage();
+                    $qrscene_id = $Usage->get_eventkey_son_info($result->eventkey);
+                    if ($qrscene_id) {
+                        foreach ($qrscene_id as $eventkey) {
+
+                            $row2 = DB::table('wx_user_info')
+                                ->where('eventkey', $eventkey)
+                                ->where('scandate', date('Y-m-d'))
+                                ->whereRaw('UNIX_TIMESTAMP(endtime)>=' . strtotime($prevtime))
+                                ->get();
+
+                            foreach ($row2 as $send_openid) {
+                                $content = new Text();
+                                $content->content = "您好，" . $result->zone_id . "景区" . $result->show_name . "的演出时间是" . $bbb . "。还没到剧场的话要抓紧了哦。\n如果您不知道剧场位置，<a href='" . $result->location_url . "'>点我</a>\n微信演出时间有时无法及时更新，以景区公示为准。";
+                                $app->staff->message($content)->by('1001@u_hengdian')->to($send_openid->wx_openid)->send();
+                            }
+                        }
+                    }
+
+                }
+                $prevtime = $bbb;
             }
         }
     }
