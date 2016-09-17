01022bf371de592e04778ec39f1792f1b1e296c2
wangke168 <wangke168@gmail.com>
2016-09-17 17:13:46 +0800

    modify queue

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -44,15 +44,13 @@ class ArticlesController extends Controller
 
     public function info()
     {
-        $app=app('wechat');
-        $eventkey = "123";
-        $row = WechatImage::focusPublished($eventkey)
-            ->orderBy('id', 'desc')
+        $row = DB::table('wx_user_info')
+            ->where('esc', '0')
+//            ->whereDate('endtime', '>=', date("Y-m-d", strtotime("-1 day")))
+            ->whereDate('endtime','>=','2016-08-28')
+            ->orderBy('id','desc')
             ->get();
-        foreach ($row as $result) {
-            $image = new Image();
-            $image->media_id = $result->media_id;
-            $app->staff->message($image)->by('1001@u_hengdian')->to('opUv9v977Njll_YHpZYMymxI_aPE')->send();
+        return $row;
         }
         /*                $row = WechatArticle::focusPublished('123')
                             ->pluck('eventkey');*/
