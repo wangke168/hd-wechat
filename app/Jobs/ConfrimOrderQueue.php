f56301c87cb47c2a3a5d08650d90df7f0274f980
wangke168 <wangke168@gmail.com>
2016-09-27 11:58:07 +0800

    confrimorder queue test

--- app/Jobs/ConfrimOrderQueue.php
+++ app/Jobs/ConfrimOrderQueue.php
@@ -16,9 +16,7 @@ class ConfrimOrderQueue extends Job implements ShouldQueue
 
     public $sellid;
     public $openid;
-    public $app;
-    public $order;
-    public $usage;
+
     
     /**
      * Create a new job instance.
@@ -27,8 +25,8 @@ class ConfrimOrderQueue extends Job implements ShouldQueue
      */
     public function __construct($openid, $sellid)
     {
-        $this->usage=new Usage();
-        $this->order=new Order();
+     /*   $this->usage=new Usage();
+        $this->order=new Order();*/
 //        $this->openid = $this->usage->authcode($openid, 'DECODE', 0);
         $this->openid=$openid;
         $this->sellid = $sellid;
@@ -41,15 +39,18 @@ class ConfrimOrderQueue extends Job implements ShouldQueue
      */
     public function handle()
     {
-        $eventkey = $this->usage->get_openid_info($this->openid)->eventkey;     //获取客人所属市场
-        $focusdate = $this->usage->get_openid_info($this->openid)->adddate;     //获取客人关注时间
-        $name=$this->order->get_order_detail($this->sellid)['name'];            //获取客人姓名
-        $phone=$this->order->get_order_detail($this->sellid)['phone'];          //获取客人电话
-        $arrive_date=$this->order->get_order_detail($this->sellid)['date'];     //获取客人与大日期
-        $city=$this->usage->MobileQueryAttribution($phone)->city;               //根据手机号获取归属地
+        $usage=new Usage();
+        $order=new Order();
+        $openId = $usage->authcode($this->openid, 'DECODE', 0);
+        $eventkey = $usage->get_openid_info($openId)->eventkey;     //获取客人所属市场
+        $focusdate = $usage->get_openid_info($openId)->adddate;     //获取客人关注时间
+        $name=$order->get_order_detail($this->sellid)['name'];            //获取客人姓名
+        $phone=$order->get_order_detail($this->sellid)['phone'];          //获取客人电话
+        $arrive_date=$order->get_order_detail($this->sellid)['date'];     //获取客人与大日期
+        $city=$usage->MobileQueryAttribution($phone)->city;               //根据手机号获取归属地
 
         DB::table('wx_order_confirm')
-            ->insert(['wx_openid'=>$this->openid,'sellid'=>$this->sellid ,'order_name'=>$name,'tel'=>$phone,
+            ->insert(['wx_openid'=>$openId,'sellid'=>$this->sellid ,'order_name'=>$name,'tel'=>$phone,
                 'arrive_date'=>$arrive_date,'eventkey'=>$eventkey,'focusdate'=>$focusdate,'city'=>$city]);
     }
     
