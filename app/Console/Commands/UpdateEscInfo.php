b7b0aa11aaaf68a23402aa99347199e69ec40c3e
wangke168 <wangke168@gmail.com>
2016-09-28 09:24:32 +0800

    wx_user_info queue test

--- app/Console/Commands/UpdateEscInfo.php
+++ app/Console/Commands/UpdateEscInfo.php
@@ -39,8 +39,8 @@ class UpdateEscInfo extends Command
     public function handle()
     {
         $rowEsc=DB::table('wx_user_esc')
-//            ->whereDate('adddate','>=',date("Y-m-d", strtotime("-1 day")))
-            ->whereDate('esc_time','>=','2016-08-28')
+            ->whereDate('adddate','>=',date("Y-m-d", strtotime("-1 day")))
+//            ->whereDate('esc_time','>=','2016-08-28')
             ->get();
         foreach ($rowEsc as $EscInfo)
         {
