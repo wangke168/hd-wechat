a698a07f4421874b147578045be5ba16512a84ab
wangke168 <wangke168@gmail.com>
2016-10-03 14:38:22 +0800

    test focus

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -51,6 +51,7 @@ class WechatController extends Controller
                             }
                             $response->insert_subscribe($openid, $eventkey, 'subscribe');       //更新openid信息
                             $response->request_focus($openid, $eventkey);                       //推送关注信息
+                            $response->request_focus_temp($openid, $eventkey);                  //黄金周景区预定推送
                             $response->make_user_tag($openid, $eventkey);                        //标签管理
                             break;
                         case 'SCAN':
