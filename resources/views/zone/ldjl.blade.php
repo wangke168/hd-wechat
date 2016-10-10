5ead708b003877228ac6d3ccbac00d4204f72414
wangke168 <wangke168@gmail.com>
2016-10-10 17:14:57 +0800

    test jssdk

--- resources/views/zone/ldjl.blade.php
+++ resources/views/zone/ldjl.blade.php
@@ -1,15 +1,15 @@
 
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
-
+    <meta charset="utf-8">
+    <title>微信JS-SDK Demo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
     <title>{{$openid}}</title>
     <link href="{{asset('css/index.css')}}" rel="stylesheet" type="text/css"/>
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <script src="{{asset('js/jquery-2.0.3.min.js')}}"></script>
 
-  
+
 
 </head>
 <body>
