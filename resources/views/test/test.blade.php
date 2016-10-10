1c0b9a1c52bb00ea44893c0dab877e183522c939
wangke168 <wangke168@gmail.com>
2016-10-10 16:10:12 +0800

    test jssdk

--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -6,20 +6,14 @@
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
     <script>
-/*        wx.ready(function () {
-
-            wx.getLocation({
-                success: function (res) {
-                    var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                    var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                    var speed = res.speed; // 速度，以米/每秒计
-                    var accuracy = res.accuracy; // 位置精度
-                },
-                cancel: function (res) {
-                    alert('用户拒绝授权获取地理位置');
-                }
-            });
-        });*/
+        wx.getLocation({
+            success: function (res) {
+                alert(JSON.stringify(res));
+            },
+            cancel: function (res) {
+                alert('用户拒绝授权获取地理位置');
+            }
+        });
     </script>
 
 </head>
@@ -546,7 +540,7 @@
         };
 
         // 7.2 获取当前地理位置
-//        document.querySelector('#getLocation').onclick = function () {
+        document.querySelector('#getLocation').onclick = function () {
             wx.getLocation({
                 success: function (res) {
                     alert(JSON.stringify(res));
@@ -555,7 +549,7 @@
                     alert('用户拒绝授权获取地理位置');
                 }
             });
-//        };
+        };
 
         // 8 界面操作接口
         // 8.1 隐藏右上角菜单
