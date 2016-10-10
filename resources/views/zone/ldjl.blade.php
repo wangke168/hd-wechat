6f2ff23f1a1358ca4d62fad6f90942c2100501be
wangke168 <wangke168@gmail.com>
2016-10-10 17:26:50 +0800

    test jssdk

--- resources/views/zone/ldjl.blade.php
+++ resources/views/zone/ldjl.blade.php
@@ -39,7 +39,56 @@
         </ul>
     </div>
     <div class="lbox_close wxapi_form">
-     
+        <h3 id="menu-basic">基础接口</h3>
+        <span class="desc">判断当前客户端是否支持指定JS接口</span>
+        <button class="btn btn_primary" id="checkJsApi">checkJsApi</button>
+
+        <h3 id="menu-share">分享接口</h3>
+        <span class="desc">获取“分享到朋友圈”按钮点击状态及自定义分享内容接口</span>
+        <button class="btn btn_primary" id="onMenuShareTimeline">onMenuShareTimeline</button>
+        <span class="desc">获取“分享给朋友”按钮点击状态及自定义分享内容接口</span>
+        <button class="btn btn_primary" id="onMenuShareAppMessage">onMenuShareAppMessage</button>
+        <span class="desc">获取“分享到QQ”按钮点击状态及自定义分享内容接口</span>
+        <button class="btn btn_primary" id="onMenuShareQQ">onMenuShareQQ</button>
+        <span class="desc">获取“分享到腾讯微博”按钮点击状态及自定义分享内容接口</span>
+        <button class="btn btn_primary" id="onMenuShareWeibo">onMenuShareWeibo</button>
+        <span class="desc">获取“分享到QZone”按钮点击状态及自定义分享内容接口</span>
+        <button class="btn btn_primary" id="onMenuShareQZone">onMenuShareQZone</button>
+
+        <h3 id="menu-image">图像接口</h3>
+        <span class="desc">拍照或从手机相册中选图接口</span>
+        <button class="btn btn_primary" id="chooseImage">chooseImage</button>
+        <span class="desc">预览图片接口</span>
+        <button class="btn btn_primary" id="previewImage">previewImage</button>
+        <span class="desc">上传图片接口</span>
+        <button class="btn btn_primary" id="uploadImage">uploadImage</button>
+        <span class="desc">下载图片接口</span>
+        <button class="btn btn_primary" id="downloadImage">downloadImage</button>
+
+        <h3 id="menu-voice">音频接口</h3>
+        <span class="desc">开始录音接口</span>
+        <button class="btn btn_primary" id="startRecord">startRecord</button>
+        <span class="desc">停止录音接口</span>
+        <button class="btn btn_primary" id="stopRecord">stopRecord</button>
+        <span class="desc">播放语音接口</span>
+        <button class="btn btn_primary" id="playVoice">playVoice</button>
+        <span class="desc">暂停播放接口</span>
+        <button class="btn btn_primary" id="pauseVoice">pauseVoice</button>
+        <span class="desc">停止播放接口</span>
+        <button class="btn btn_primary" id="stopVoice">stopVoice</button>
+        <span class="desc">上传语音接口</span>
+        <button class="btn btn_primary" id="uploadVoice">uploadVoice</button>
+        <span class="desc">下载语音接口</span>
+        <button class="btn btn_primary" id="downloadVoice">downloadVoice</button>
+
+        <h3 id="menu-smart">智能接口</h3>
+        <span class="desc">识别音频并返回识别结果接口</span>
+        <button class="btn btn_primary" id="translateVoice">translateVoice</button>
+
+        <h3 id="menu-device">设备信息接口</h3>
+        <span class="desc">获取网络状态接口</span>
+        <button class="btn btn_primary" id="getNetworkType">getNetworkType</button>
+
         <h3 id="menu-location">地理位置接口</h3>
         <span class="desc">使用微信内置地图查看位置接口</span>
         <button class="btn btn_primary" id="openLocation">openLocation</button>
