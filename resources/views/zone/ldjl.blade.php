f942ed543e908e4550a72b5f579ee450670b66ee
wangke168 <wangke168@gmail.com>
2016-10-10 17:25:17 +0800

    test jssdk

--- resources/views/zone/ldjl.blade.php
+++ resources/views/zone/ldjl.blade.php
@@ -129,16 +129,7 @@
             'downloadImage',
             'getNetworkType',
             'openLocation',
-            'getLocation',
-            'hideOptionMenu',
-            'showOptionMenu',
-            'closeWindow',
-            'scanQRCode',
-            'chooseWXPay',
-            'openProductSpecificView',
-            'addCard',
-            'chooseCard',
-            'openCard'), false) ?>);
+            'getLocation'), false) ?>);
 
     wx.ready(function () {
         // 1 判断当前版本是否支持指定 JS 接口，支持批量判断
@@ -537,176 +528,6 @@
         });
 //        };
 
-        // 8 界面操作接口
-        // 8.1 隐藏右上角菜单
-        document.querySelector('#hideOptionMenu').onclick = function () {
-            wx.hideOptionMenu();
-        };
-
-        // 8.2 显示右上角菜单
-        document.querySelector('#showOptionMenu').onclick = function () {
-            wx.showOptionMenu();
-        };
-
-        // 8.3 批量隐藏菜单项
-        document.querySelector('#hideMenuItems').onclick = function () {
-            wx.hideMenuItems({
-                menuList: [
-                    'menuItem:readMode', // 阅读模式
-                    'menuItem:share:timeline', // 分享到朋友圈
-                    'menuItem:copyUrl' // 复制链接
-                ],
-                success: function (res) {
-                    alert('已隐藏“阅读模式”，“分享到朋友圈”，“复制链接”等按钮');
-                },
-                fail: function (res) {
-                    alert(JSON.stringify(res));
-                }
-            });
-        };
-
-        // 8.4 批量显示菜单项
-        document.querySelector('#showMenuItems').onclick = function () {
-            wx.showMenuItems({
-                menuList: [
-                    'menuItem:readMode', // 阅读模式
-                    'menuItem:share:timeline', // 分享到朋友圈
-                    'menuItem:copyUrl' // 复制链接
-                ],
-                success: function (res) {
-                    alert('已显示“阅读模式”，“分享到朋友圈”，“复制链接”等按钮');
-                },
-                fail: function (res) {
-                    alert(JSON.stringify(res));
-                }
-            });
-        };
-
-        // 8.5 隐藏所有非基本菜单项
-        document.querySelector('#hideAllNonBaseMenuItem').onclick = function () {
-            wx.hideAllNonBaseMenuItem({
-                success: function () {
-                    alert('已隐藏所有非基本菜单项');
-                }
-            });
-        };
-
-        // 8.6 显示所有被隐藏的非基本菜单项
-        document.querySelector('#showAllNonBaseMenuItem').onclick = function () {
-            wx.showAllNonBaseMenuItem({
-                success: function () {
-                    alert('已显示所有非基本菜单项');
-                }
-            });
-        };
-
-        // 8.7 关闭当前窗口
-        document.querySelector('#closeWindow').onclick = function () {
-            wx.closeWindow();
-        };
-
-        // 9 微信原生接口
-        // 9.1.1 扫描二维码并返回结果
-        document.querySelector('#scanQRCode0').onclick = function () {
-            wx.scanQRCode();
-        };
-        // 9.1.2 扫描二维码并返回结果
-        document.querySelector('#scanQRCode1').onclick = function () {
-            wx.scanQRCode({
-                needResult: 1,
-                desc: 'scanQRCode desc',
-                success: function (res) {
-                    alert(JSON.stringify(res));
-                }
-            });
-        };
-
-        // 10 微信支付接口
-        // 10.1 发起一个支付请求
-        document.querySelector('#chooseWXPay').onclick = function () {
-            // 注意：此 Demo 使用 2.7 版本支付接口实现，建议使用此接口时参考微信支付相关最新文档。
-            wx.chooseWXPay({
-                timestamp: 1414723227,
-                nonceStr: 'noncestr',
-                package: 'addition=action_id%3dgaby1234%26limit_pay%3d&bank_type=WX&body=innertest&fee_type=1&input_charset=GBK&notify_url=http%3A%2F%2F120.204.206.246%2Fcgi-bin%2Fmmsupport-bin%2Fnotifypay&out_trade_no=1414723227818375338&partner=1900000109&spbill_create_ip=127.0.0.1&total_fee=1&sign=432B647FE95C7BF73BCD177CEECBEF8D',
-                signType: 'SHA1', // 注意：新版支付接口使用 MD5 加密
-                paySign: 'bd5b1933cda6e9548862944836a9b52e8c9a2b69'
-            });
-        };
-
-        // 11.3  跳转微信商品页
-        document.querySelector('#openProductSpecificView').onclick = function () {
-            wx.openProductSpecificView({
-                productId: 'pDF3iY_m2M7EQ5EKKKWd95kAxfNw',
-                extInfo: '123'
-            });
-        };
-
-        // 12 微信卡券接口
-        // 12.1 添加卡券
-        document.querySelector('#addCard').onclick = function () {
-            wx.addCard({
-                cardList: [
-                    {
-                        cardId: 'p2e-YuLyjE-EbQpZd3-F2-ayqAfQ',
-                        cardExt: '{"code": "", "openid": "", "timestamp": "1474357424", "signature":"46343759d52d62260a9dca0b6a4c504fe2fe8d62"}'
-                    },
-                    {
-                        cardId: 'p2e-YuPji_LBT34qx0xSCE4LKj-E',
-                        cardExt: '{"code": "", "openid": "", "timestamp": "1474353655", "signature":"fc056a3f1276cb8ab78c6776ccb64de9b983bf63"}'
-                    }
-                ],
-                success: function (res) {
-                    alert('已添加卡券：' + JSON.stringify(res.cardList));
-                },
-                cancel: function (res) {
-                    alert(JSON.stringify(res))
-                }
-            });
-        };
-
-        var codes = [];
-        // 12.2 选择卡券
-        document.querySelector('#chooseCard').onclick = function () {
-            wx.chooseCard({
-                cardSign: '6caa49f4a5af3d64ac247e1f563e5b5eb94619ad',
-                timestamp: 1437997723,
-                nonceStr: 'k0hGdSXKZEj3Min5',
-                success: function (res) {
-                    res.cardList = JSON.parse(res.cardList);
-                    encrypt_code = res.cardList[0]['encrypt_code'];
-                    alert('已选择卡券：' + JSON.stringify(res.cardList));
-                    decryptCode(encrypt_code, function (code) {
-                        codes.push(code);
-                    });
-                },
-                cancel: function (res) {
-                    alert(JSON.stringify(res))
-                }
-            });
-        };
-
-        // 12.3 查看卡券
-        document.querySelector('#openCard').onclick = function () {
-            if (codes.length < 1) {
-                alert('请先使用 chooseCard 接口选择卡券。');
-                return false;
-            }
-            var cardList = [];
-            for (var i = 0; i < codes.length; i++) {
-                cardList.push({
-                    cardId: 'pDF3iY9tv9zCGCj4jTXFOo1DxHdo',
-                    code: codes[i]
-                });
-            }
-            wx.openCard({
-                cardList: cardList,
-                cancel: function (res) {
-                    alert(JSON.stringify(res))
-                }
-            });
-        };
-
         var shareData = {
             title: '微信JS-SDK Demo',
             desc: '微信JS-SDK,帮助第三方为用户提供更优质的移动web服务',
@@ -716,13 +537,7 @@
         wx.onMenuShareAppMessage(shareData);
         wx.onMenuShareTimeline(shareData);
 
-        function decryptCode(code, callback) {
-            $.getJSON('/jssdk/decrypt_code.php?code=' + encodeURI(code), function (res) {
-                if (res.errcode == 0) {
-                    codes.push(res.code);
-                }
-            });
-        }
+     
     });
 
     wx.error(function (res) {
