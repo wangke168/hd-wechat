c9b21c2578444bb14c3146a6945573a4a49cdc20
wangke168 <wangke168@gmail.com>
2016-10-10 17:30:40 +0800

    test jssdk

--- resources/views/zone/ldjl.blade.php
+++ resources/views/zone/ldjl.blade.php
@@ -127,7 +127,7 @@
             'previewImage',
             'uploadImage',
             'downloadImage',
-            'getNetworkType',
+            
             'openLocation',
             'getLocation'), false) ?>);
 
