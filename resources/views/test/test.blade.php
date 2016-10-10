e231be5fe839cac00d65f9b4660c89418c37ce43
wangke168 <wangke168@gmail.com>
2016-10-10 16:15:35 +0800

    test jssdk

--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -546,7 +546,12 @@
 //        document.querySelector('#getLocation').onclick = function () {
             wx.getLocation({
                 success: function (res) {
-                    alert(JSON.stringify(res));
+                    var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                    var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                    var speed = res.speed; // 速度，以米/每秒计
+                    var accuracy = res.accuracy; // 位置精度
+                    alert(latitude);
+//                    alert(JSON.stringify(res));
                 },
                 cancel: function (res) {
                     alert('用户拒绝授权获取地理位置');
