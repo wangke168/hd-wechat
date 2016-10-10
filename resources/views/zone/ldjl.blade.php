28e2d6c3abfef29d8fcdc92b0236622e5304a510
wangke168 <wangke168@gmail.com>
2016-10-10 17:32:13 +0800

    test jssdk

--- resources/views/zone/ldjl.blade.php
+++ resources/views/zone/ldjl.blade.php
@@ -85,7 +85,10 @@
         <span class="desc">识别音频并返回识别结果接口</span>
         <button class="btn btn_primary" id="translateVoice">translateVoice</button>
 
- 
+        <h3 id="menu-device">设备信息接口</h3>
+        <span class="desc">获取网络状态接口</span>
+        <button class="btn btn_primary" id="getNetworkType">getNetworkType</button>
+
         <h3 id="menu-location">地理位置接口</h3>
         <span class="desc">使用微信内置地图查看位置接口</span>
         <button class="btn btn_primary" id="openLocation">openLocation</button>
