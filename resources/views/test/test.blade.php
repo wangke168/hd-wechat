370ca76f01f2a524403042df0c9275ffedea5807
wangke168 <wangke168@gmail.com>
2016-10-10 15:55:12 +0800

    test jssdk

--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -6,17 +6,20 @@
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
     <script>
-        wx.ready(function () {
+/*        wx.ready(function () {
 
             wx.getLocation({
                 success: function (res) {
-                    alert(JSON.stringify(res));
+                    var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+                    var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+                    var speed = res.speed; // 速度，以米/每秒计
+                    var accuracy = res.accuracy; // 位置精度
                 },
                 cancel: function (res) {
                     alert('用户拒绝授权获取地理位置');
                 }
             });
-        });
+        });*/
     </script>
 
 </head>
@@ -181,10 +184,15 @@
             wx.checkJsApi({
                 jsApiList: [
                     'getNetworkType',
-                    'previewImage'
+                    'previewImage',
+                    'getLocation'
+
                 ],
                 success: function (res) {
-                    alert(JSON.stringify(res));
+//                    alert(JSON.stringify(res));
+                    if (res.checkResult.getLocation == false) {
+                        alert('你的微信版本太低，不支持微信JS接口，请升级到最新的微信版本！');
+                        return;
                 }
             });
         };
