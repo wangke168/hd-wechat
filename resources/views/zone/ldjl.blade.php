a33f410fe1ff9e7cccc260cebb78166c65e95641
wangke168 <wangke168@gmail.com>
2016-10-11 10:37:38 +0800

    test jssdk

--- resources/views/zone/ldjl.blade.php
+++ resources/views/zone/ldjl.blade.php
@@ -148,7 +148,7 @@
     })
     /*取号*/
     function getqh() {
-        if (document.getElementById("info").innerHTML=="您所在位置:龙帝惊临取号处") {
+        if (document.getElementById("info").innerHTML!="您所在位置:龙帝惊临取号处") {
             $(".overdiv").show(1)
                     .find(".closebtn").show(1)
                     .nextAll("span").html("您好，只有在龙帝惊临取号范围才能预约,如果您确认在景区请点击点位按钮重新获取您的位置。");
