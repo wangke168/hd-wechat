55e6babe59a2acfa5ac7ac97fd210342130312fd
wangke168 <wangke168@gmail.com>
2016-10-25 15:25:32 +0800

    article.blade test

--- resources/views/articles/detail.blade.php
+++ resources/views/articles/detail.blade.php
@@ -85,7 +85,7 @@
                 imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
                 trigger: function (res) {
                     // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
-                    alert('用户点击分享到朋友圈');
+                    //alert('用户点击分享到朋友圈');
                 },
                 success: function (res) {
                     alert('已分享');
