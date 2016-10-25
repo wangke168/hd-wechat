8a5e79d93201bc13e880d145ef0ad45b8d59260f
wangke168 <wangke168@gmail.com>
2016-10-25 14:46:26 +0800

    article.blade test

--- resources/views/articles/detail.blade.php
+++ resources/views/articles/detail.blade.php
@@ -62,8 +62,7 @@
 
     <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
     <script type="text/javascript" charset="utf-8">
-        wx.config(<?php echo $js->config(array('checkJsApi',
-            'onMenuShareTimeline',
+        wx.config(<?php echo $js->config(array('onMenuShareTimeline',
             'onMenuShareAppMessage',
             'onMenuShareQQ',
             'onMenuShareWeibo',
@@ -212,13 +211,7 @@
         wx.error(function (res) {
             alert(res.errMsg);
         });
-
-
-
-        /*关闭按钮*/
-        function closeoverdiv() {
-            $(".overdiv").hide(1);
-        }
+        
     </script>
 
     </html>
