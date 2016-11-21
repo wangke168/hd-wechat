8fb0872ac1a5c221def3a16d1243e2782bf31463
wangke168 <wangke168@gmail.com>
2016-11-21 14:47:27 +0800

    cancel request_focus_temp

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -52,7 +52,7 @@ class WechatController extends Controller
                             $response->insert_subscribe($openid, $eventkey, 'subscribe'); //更新openid信息
                             $response->request_focus($openid, $eventkey); //推送关注信息
 
-                            $response->request_focus_temp($openid, $eventkey); //黄金周景区预定推送
+                        //    $response->request_focus_temp($openid, $eventkey); //黄金周景区预定推送
                             
                             $response->make_user_tag($openid, $eventkey); //标签管理
                             break;
