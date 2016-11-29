9fac15d86e2a4ab8c303b9440381c6a6e520f218
wangke168 <wangke168@gmail.com>
2016-11-29 09:57:16 +0800

    detail

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -34,36 +34,38 @@ class ArticlesController extends Controller
 
     public function __construct(Application $app)
     {
-        $this->app = $app;
-        $this->js = $this->app->js;
-        $this->count = new Count();
-        $this->usage = new Usage();
+        $this->app=$app;
+        $this->js=$this->app->js;
+        $this->count=new Count();
+        $this->usage=new Usage();
     }
 
-    public function second_article($sellid, $openid, $info_id)
+    public function second_article($sellid,$openid,$info_id)
     {
         $usage = new Usage();
         $openid = $usage->authcode($openid, 'DECODE', 0);
 
         //设置se_info_send阅读
         DB::table('se_info_send')
-            ->where('sellid', $sellid)
-            ->where('wx_openid', $openid)
-            ->where('info_id', $info_id)
-            ->update(['is_read' => 1, 'readtime' => Carbon::now()]);
+            ->where('sellid',$sellid)
+            ->where('wx_openid',$openid)
+            ->where('info_id',$info_id)
+            ->update(['is_read'=>1,'readtime'=>Carbon::now()]);
 
         //增加阅读数
         DB::table('se_info_detail')
-            ->where('id', $info_id)
+            ->where('id',$info_id)
             ->increment('hits');
 
         //找出对应url并跳转
-        $row = DB::table('se_info_detail')
-            ->where('id', $info_id)
+        $row=DB::table('se_info_detail')
+            ->where('id',$info_id)
             ->first();
-        $url = $row->article_url;
+        $url=$row->article_url;
         return redirect($url);
     }
+
+
 
 
     public function index()
@@ -81,34 +83,32 @@ class ArticlesController extends Controller
 
     public function detail(Request $request)
     {
-        $id = $request->input('id');
-        $wxnumber = $request->input('wxnumber');
-        $wxnumber = $this->usage->authcode($wxnumber, 'DECODE', 0);
-        /*        $openid=$request->input('openid');
+        $id=$request->input('id');
+        $wxnumber=$request->input('wxnumber');
+        $openid=$this->usage->authcode($wxnumber,'DECODE',0);
+ /*       $openid=$request->input('openid');
 
-                if ($wxnumber)
-                {
-                    $openid=$wxnumber;
-                }
-                */
-        $temp_rando = mt_rand();
+        if ($wxnumber)
+        {
+            $openid=$wxnumber;
+        }*/
 
-        DB::table('a_test')
-            ->insert(['test' => $temp_rando, 'wx_openid' => $wxnumber]);
+        $article = WechatArticle::find($id);
+        if (!$article || $article->online=='0' ||$article->enddate<Carbon::now())
+        {
+            abort(404);
+        }
+        else {
+            $temp_rando=mt_rand();
 
-        /*        $article = WechatArticle::find($id);
-                if (!$article || $article->online=='0' ||$article->enddate<Carbon::now())
-                {
-                    abort(404);
-                }
-                else {
+            DB::table('a_test')
+                ->insert(['test'=>$temp_rando,'wx_openid'=>$openid]);
 
+            $this->count->add_article_hits($id);
+            $this->count->insert_hits($id,$openid);
 
-                    $this->count->add_article_hits($id);
-                    $this->count->insert_hits($id,$openid);
-
-                    return view('articles.detail', compact('article', 'id', 'openid'));
-                }*/
+            return view('articles.detail', compact('article', 'id', 'openid'));
+        }
     }
 
     public function info()
