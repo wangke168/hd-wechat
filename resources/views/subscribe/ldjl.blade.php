1cbc04c6892629b618c7701f18b533bb807fc0d8
wangke168 <wangke168@gmail.com>
2016-12-22 15:08:52 +0800

    test

--- resources/views/subscribe/ldjl.blade.php
+++ resources/views/subscribe/ldjl.blade.php
@@ -14,7 +14,8 @@ $project_id = "1";
 //$signPackage = $jssdk->GetSignPackage();
 ?>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="zh-hans">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
