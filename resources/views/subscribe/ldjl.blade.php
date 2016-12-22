050fcadcbe36b53a83eb11e08ac76f0d0b41fd64
wangke168 <wangke168@gmail.com>
2016-12-22 15:17:01 +0800

    test

--- resources/views/subscribe/ldjl.blade.php
+++ resources/views/subscribe/ldjl.blade.php
@@ -1,15 +1,31 @@
-
 <!DOCTYPE html>
 <html lang="zh-hans">
-<head>
+<head id="Head2">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
-
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
+    <meta name="MobileOptimized" content="320"/>
+    <meta name="copyright" content="Copyright &copy; 2013 hengdianworld.com Inc. All Rights Reserved."/>
+    <meta name="description" content="掌上横店！掌上横店是国家5A级旅游景区横店影视城的移动门户- m.hengdianworld.com"/>
+    <meta name="keywords" content="掌上横店,掌上横店影视城,横店影视城手机版网站"/>
     <title></title>
-    <link href="{{asset('css/index.css')}}" rel="stylesheet" type="text/css"/>
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <script src="{{asset('js/jquery-2.0.3.min.js')}}"></script>
+    <link href="{{asset('css/mbcss.css')}}" rel="stylesheet" type="text/css"/>
+    <style type="text/css">
+        img {
+            max-width: 100%;
+        }
+    </style>
+    <script src="{{asset('js/jquery-1.10.2.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/divheight.js')}}"></script>
 
+    <script>
+        $(function () {
+            var awidth = parseInt($(document).width());//获取屏幕的宽度
+            $("iframe").css({"width": "100%"})  //设置宽度
+                    .height(awidth / 4 * 3);  //设置高度
+        })
+    </script>
 
 </head>
 <body>
