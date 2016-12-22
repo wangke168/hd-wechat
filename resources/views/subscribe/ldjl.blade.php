2f1431a18976b394d63bee39016b5131e0ab98c1
wangke168 <wangke168@gmail.com>
2016-12-22 15:16:13 +0800

    test

--- resources/views/subscribe/ldjl.blade.php
+++ resources/views/subscribe/ldjl.blade.php
@@ -22,15 +22,18 @@
 <script type="text/javascript" charset="utf-8">
     wx.config(<?php echo $js->config(array('onMenuShareTimeline',
             'onMenuShareAppMessage',
-            'getLocation'), true) ?>);
+            'onMenuShareQQ',
+            'onMenuShareWeibo',
+            'getLocation',
+            'onMenuShareQZone'), false) ?>);
 
     wx.ready(function () {
 
         wx.onMenuShareAppMessage({
-            title: '龙帝惊临智能预约系统', // 分享标题
-            desc: '龙帝惊临智能预约系统', // 分享描述
-            link: 'link', // 分享链接
-            imgUrl: 'imgUrl', // 分享图标
+            title: '', // 分享标题
+            desc: '', // 分享描述
+            link: '', // 分享链接
+            imgUrl: '', // 分享图标
             type: '', // 分享类型,music、video或link，不填默认为link
             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
             success: function () {
@@ -42,9 +45,9 @@
             }
         });
         wx.onMenuShareTimeline({
-            title: '龙帝惊临智能预约系统',
-            link: '龙帝惊临智能预约系统',
-            imgUrl: 'imgUrl',
+            title: '',
+            link: '',
+            imgUrl: '',
             success: function (res) {
 //                alert('已分享');
                 $.get('/count/addresp/');
@@ -54,36 +57,14 @@
             }
 
         });
-        // 7.2 获取当前地理位置
         wx.getLocation({
             success: function (res) {
                 alert(JSON.stringify(res));
-               /* var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-                var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-                var speed = res.speed; // 速度，以米/每秒计
-                var accuracy = res.accuracy; // 位置精度
-                */
-                $(".info").html("");
-                var weidu = res.latitude; //维度
-                var jingdu = res.longitude;//经度
-                if (weidu > 29.136 && weidu < 29.140 && jingdu > 120.306 && jingdu < 120.315) {
-                    $(".info").html("您所在位置:龙帝惊临取号处");
-                }
-                /*影视城位置以下可注释*/
-                else if (weidu > 29.154 && weidu < 29.1549 && jingdu > 120.312 && jingdu < 120.320) {
-                    $(".info").html("您所在位置:横店影视城有限公司");
-                }
-                /*影视城位置以上可注释*/
-                else {
-                    $(".info").html("您不在龙帝惊临取号范围");
-                }
-
             },
             cancel: function (res) {
                 alert('用户拒绝授权获取地理位置');
             }
         });
-
     });
 
     wx.error(function (res) {
@@ -92,4 +73,5 @@
 
 
 </script>
+
 </html>
