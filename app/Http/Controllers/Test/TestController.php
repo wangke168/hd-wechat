7bd852a15e6eb6aa9a64b11a32a885e9ef76c38d
wangke168 <wangke168@gmail.com>
2017-01-17 20:04:09 +0800

    test

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -82,7 +82,7 @@ class TestController extends Controller
     {
         for ($k='1366'; $k <'1368'; $k++) { 
             $i=$k-1365;
-            $qrscene_name='永康酒店'&$i;
+            $qrscene_name='永康酒店'.$i;
              $row=DB::table('wx_qrscene_info')
         ->insert(['classid'=>'1','qrscene_id'=>$k,'qrscene_name'=>$qrscene_name]);
         // return $row;
