a15cd288540324d06776b41cb085241e47428e26
wangke168 <wangke168@gmail.com>
2017-01-17 20:21:11 +0800

    test

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -87,8 +87,8 @@ class TestController extends Controller
         ->insert(['classid'=>'1','qrscene_id'=>$k,'qrscene_name'=>$qrscene_name]);
         // return $row;
         }
-        for ($k='1396'; $k <'1416'; $k++) { 
-            $i=$k-1395;
+   /*     for ($k='1396'; $k <'1416'; $k++) { 
+           $i=$k-1395;
             $qrscene_name='金华酒店'.$i;
              $row=DB::table('wx_qrscene_info')
         ->insert(['classid'=>'1','qrscene_id'=>$k,'qrscene_name'=>$qrscene_name]);
@@ -101,6 +101,7 @@ class TestController extends Controller
         ->insert(['classid'=>'1','qrscene_id'=>$k,'qrscene_name'=>$qrscene_name]);
         // return $row;
         }
+        */
         return $k;
        
     }
