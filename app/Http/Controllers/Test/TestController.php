85c767e1b9776833c594ae34b6294c2b81f66349
wangke168 <wangke168@gmail.com>
2017-01-21 13:40:25 +0800

    test

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -115,13 +115,14 @@ class TestController extends Controller
 
     public function cache()
     {
-//        $memcached=new \Memcached();
+        $memcached=new \Memcache();
+
 //        $memcached = new Memcached();
 //        $memcached->addServer('localhost', 11211);
 
-        $cacheDriver = new MemcachedCache();
+//        $cacheDriver = new MemcachedCache();
 //        $cacheDriver->setMemcached($memcached);
-        $cacheDriver->save('cache_id', '123456789');
+//        $cacheDriver->save('cache_id', '123456789');
       //  return $cacheDriver->fetch('cache_id');
      //   return $cacheDriver->get('cache_id');
         //\Cache::add('temp','123456','60');
