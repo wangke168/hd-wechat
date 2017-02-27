b133414b4fea896656ebea7b6f7c77e60678c6ee
wangke168 <wangke168@gmail.com>
2017-02-27 14:35:13 +0800

    test

--- resources/views/zone/ldjl.blade.php
+++ resources/views/zone/ldjl.blade.php
@@ -56,7 +56,7 @@ $project_id="1";
 </body>
 
 
-<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
+<script src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" charset="utf-8">
     wx.config(<?php echo $js->config(array('checkJsApi',
             'onMenuShareTimeline',
