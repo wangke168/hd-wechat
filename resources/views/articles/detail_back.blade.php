427966bfd0779d6b7573240340454414e792097e
wangke168 <wangke168@gmail.com>
2017-02-27 10:27:40 +0800

    test

--- resources/views/articles/detail_back.blade.php
+++ resources/views/articles/detail_back.blade.php
@@ -97,7 +97,7 @@
     }
 ?>
 
-<script src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" charset="utf-8">
     wx.config(<?php echo $js->config(array('onMenuShareTimeline',
             'onMenuShareAppMessage',
