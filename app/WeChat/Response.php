90da543df6a3b9d82bf3f18fabbc5840887df807
wangke168 <wangke168@gmail.com>
2017-03-10 10:50:38 +0800

    test

--- app/WeChat/Response.php
+++ app/WeChat/Response.php
@@ -71,7 +71,7 @@ class Response
                     $this->server->setMessageHandler(function ($message) {
                         $transfer = new \EasyWeChat\Message\Transfer();
 
-                        $transfer->to('kf2004@u_hengdian');// 或者 $transfer->to($account);
+                        $transfer->account('kf2004@u_hengdian');// 或者 $transfer->to($account);
 
                         return $transfer;
                     });
