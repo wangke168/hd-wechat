ccd65833750e02c4a9662c01a654f16de096d9e4
wangke168 <wangke168@gmail.com>
2017-03-24 17:12:54 +0800

    test

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -246,7 +246,7 @@ class OrderController extends Controller
 
         $this->notice->uses($templateId)->withUrl($url)->andData($data)->andReceiver($userId)->send();
 
-        $this->app->staff->message($content)->to($openid)->send();
+  //      $this->app->staff->message($content)->to($openid)->send();
 //        $app->staff->message($news)->to($openid)->send();
     }
 }
