8bbd100adb8175ea248120b9f92d36ebf4f74e94
wangke168 <wangke168@gmail.com>
2017-03-24 17:21:16 +0800

    test

--- app/WeChat/SecondSell.php
+++ app/WeChat/SecondSell.php
@@ -19,35 +19,35 @@ class SecondSell
 
         $usage = new Usage();
         $wxnumber = $usage->authcode($openid, 'ENCODE', 0);
-        $content='';
-       /* $rows = DB::table('wx_article_se')
-            ->where('online', '1')
-            ->where('target', '1')
-            ->whereDate('startdate', '<=', date('Y-m-d'))
-            ->whereDate('enddate', '>=', date('Y-m-d'))
-            ->orderBy('priority', 'asc')
-            ->get();
-        foreach ($rows as $row) {
-            $news = new News();
-            $news->title = $row->title;
-            $news->description = $row->description;
+        // $content='';
+        /* $rows = DB::table('wx_article_se')
+             ->where('online', '1')
+             ->where('target', '1')
+             ->whereDate('startdate', '<=', date('Y-m-d'))
+             ->whereDate('enddate', '>=', date('Y-m-d'))
+             ->orderBy('priority', 'asc')
+             ->get();
+         foreach ($rows as $row) {
+             $news = new News();
+             $news->title = $row->title;
+             $news->description = $row->description;
 
-            if ($row->url) {
-                $url = $row->url;
-            } else {
-                $url = "http://" . $_SERVER['HTTP_HOST'] . "/article/detail?action=se&id=" . $row->id . "&wxnumber=" . $openid;
-            }
-            $news->url = $url;
-            $news->image = $row->pic_url;
-            $content[] = $news;
-            $info_ids[] = $row->id;
-        }*/
+             if ($row->url) {
+                 $url = $row->url;
+             } else {
+                 $url = "http://" . $_SERVER['HTTP_HOST'] . "/article/detail?action=se&id=" . $row->id . "&wxnumber=" . $openid;
+             }
+             $news->url = $url;
+             $news->image = $row->pic_url;
+             $content[] = $news;
+             $info_ids[] = $row->id;
+         }*/
 
         switch ($type) {
             case 'ticket':
                 $rows = DB::table('wx_article_se')
                     ->where('online', '1')
-                    ->whereIn('target', [1,2])
+                    ->whereIn('target', [1, 2])
                     ->orderBy('priority', 'asc')
                     ->get();
                 foreach ($rows as $row) {
@@ -58,7 +58,7 @@ class SecondSell
                     if ($row->url) {
                         $url = $row->url;
                     } else {
-                        $url = "http://" . $_SERVER['HTTP_HOST'] . "/article/detail?type=se&sellid=".$sellid."&id=" . $row->id . "&wxnumber=" . $wxnumber;
+                        $url = "http://" . $_SERVER['HTTP_HOST'] . "/article/detail?type=se&sellid=" . $sellid . "&id=" . $row->id . "&wxnumber=" . $wxnumber;
                     }
                     $news->url = $url;
                     $news->image = $pic_url;
@@ -86,92 +86,96 @@ class SecondSell
                     $info_ids[]=$row->id;
                 }*/
 
-       /* switch ($type) {
-            case 'ticket':
-                $rows = DB::table('se_info_detail')
-                    ->where('online', '1')
-                    ->where('is_all', '0')
-                    ->orderBy('sequence', 'asc')
-                    ->get();
-                foreach ($rows as $row) {
-                    if ($row->zone) {
-                        if (strpos($order_info, $row->zone) !== false) {
-                            $news = new News();
-                            $news->title = $row->title;
-                            $news->description = $row->description;
-//                            $news->url = $row->article_url;
-                            $news->url = $url . $row->id;
-                            $news->image = $row->pic_url;
-                            $content[] = $news;
-                            $info_ids[] = $row->id;
-                        }
-                    }
-                }
-                break;
-            case 'inclusive':
-                $rows = DB::table('se_info_detail')
-                    ->where('online', '1')
-                    ->where('is_all', '0')
-                    ->orderBy('sequence', 'asc')
-                    ->get();
-                foreach ($rows as $row) {
-                    if ($row->zone) {
-                        if (strpos($order_info, $row->zone) !== false) {
-                            $news = new News();
-                            $news->title = $row->title;
-                            $news->description = $row->description;
-//                            $news->url = $row->article_url;
-                            $news->url = $url . $row->id;
-                            $news->image = $row->pic_url;
-                            $content[] = $news;
-                            $info_ids[] = $row->id;
-                        }
-                    }
-                    if ($row->hotel) {
-                        if (strpos($order_info, $row->hotel) !== false) {
-                            $news = new News();
-                            $news->title = $row->title;
-                            $news->description = $row->description;
-//                            $news->url = $row->article_url;
-                            $news->url = $url . $row->id;
-                            $news->image = $row->pic_url;
-                            $content[] = $news;
-                            $info_ids[] = $row->id;
-                        }
-                    }
-                }
+        /* switch ($type) {
+             case 'ticket':
+                 $rows = DB::table('se_info_detail')
+                     ->where('online', '1')
+                     ->where('is_all', '0')
+                     ->orderBy('sequence', 'asc')
+                     ->get();
+                 foreach ($rows as $row) {
+                     if ($row->zone) {
+                         if (strpos($order_info, $row->zone) !== false) {
+                             $news = new News();
+                             $news->title = $row->title;
+                             $news->description = $row->description;
+ //                            $news->url = $row->article_url;
+                             $news->url = $url . $row->id;
+                             $news->image = $row->pic_url;
+                             $content[] = $news;
+                             $info_ids[] = $row->id;
+                         }
+                     }
+                 }
+                 break;
+             case 'inclusive':
+                 $rows = DB::table('se_info_detail')
+                     ->where('online', '1')
+                     ->where('is_all', '0')
+                     ->orderBy('sequence', 'asc')
+                     ->get();
+                 foreach ($rows as $row) {
+                     if ($row->zone) {
+                         if (strpos($order_info, $row->zone) !== false) {
+                             $news = new News();
+                             $news->title = $row->title;
+                             $news->description = $row->description;
+ //                            $news->url = $row->article_url;
+                             $news->url = $url . $row->id;
+                             $news->image = $row->pic_url;
+                             $content[] = $news;
+                             $info_ids[] = $row->id;
+                         }
+                     }
+                     if ($row->hotel) {
+                         if (strpos($order_info, $row->hotel) !== false) {
+                             $news = new News();
+                             $news->title = $row->title;
+                             $news->description = $row->description;
+ //                            $news->url = $row->article_url;
+                             $news->url = $url . $row->id;
+                             $news->image = $row->pic_url;
+                             $content[] = $news;
+                             $info_ids[] = $row->id;
+                         }
+                     }
+                 }
 
-                break;
-            case
-            'hotel':
-                $rows = DB::table('se_info_detail')
-                    ->where('online', '1')
-                    ->where('is_all', '0')
-                    ->orderBy('sequence', 'asc')
-                    ->get();
-                foreach ($rows as $row)
-                    if ($row->hotel) {
-                        if (strpos($order_info, $row->hotel) !== false) {
-                            $news = new News();
-                            $news->title = $row->title;
-                            $news->description = $row->description;
-//                            $news->url = $row->article_url;
-                            $news->url = $url . $row->id;
-                            $news->image = $row->pic_url;
-                            $content[] = $news;
-                            $info_ids[] = $row->id;
-                        }
-                    }
-                break;
-        }*/
+                 break;
+             case
+             'hotel':
+                 $rows = DB::table('se_info_detail')
+                     ->where('online', '1')
+                     ->where('is_all', '0')
+                     ->orderBy('sequence', 'asc')
+                     ->get();
+                 foreach ($rows as $row)
+                     if ($row->hotel) {
+                         if (strpos($order_info, $row->hotel) !== false) {
+                             $news = new News();
+                             $news->title = $row->title;
+                             $news->description = $row->description;
+ //                            $news->url = $row->article_url;
+                             $news->url = $url . $row->id;
+                             $news->image = $row->pic_url;
+                             $content[] = $news;
+                             $info_ids[] = $row->id;
+                         }
+                     }
+                 break;
+         }*/
 
-        if($info_ids) {
+        if ($info_ids) {
             foreach ($info_ids as $info_id) {
                 DB::table('se_info_send')
                     ->insert(['wx_openid' => $openid, 'sellid' => $sellid, 'info_id' => $info_id]);
             }
         }
-        return $content;
+        if ($content) {
+            return $content;
+        } else {
+            return null;
+        }
     }
 
 }
