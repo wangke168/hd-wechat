1fc8bb765c0cbc6b26f1ca96d0fd97e2e1f9a438
wangke168 <wangke168@gmail.com>
2017-09-02 13:51:23 +0800

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -51,8 +51,8 @@ class Kernel extends ConsoleKernel
      //       ->cron('*/20 9-16 * * *');
 
         /*更新wx_user_info里的信息*/
-        $schedule->command('UpdateOpenidInfo')
-            ->dailyAt('11:00');
+   //     $schedule->command('UpdateOpenidInfo')
+     //       ->dailyAt('11:00');
 
         /*更新wx_click_hits中的eventkey*/
      //   $schedule->command('UpdateClickInfo')
