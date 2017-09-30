152d36b29540102d29bba68a3bb0d12f34ed26fc
wangke168 <wangke168@gmail.com>
2017-09-30 15:38:55 +0800

    test

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -167,7 +167,7 @@ class MenuController extends Controller
                 "sub_button" => [
                     [
                         "type" => "click",
-                        "name" => "门票预订",
+                        "name" => "门票购买",
                         "key" => "97"
                     ],
                     [
