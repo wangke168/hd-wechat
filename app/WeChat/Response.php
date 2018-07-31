a779201e75d4cdcad8918164cb0f14974b8b7777
wangke168 <wangke168@gmail.com>
2018-07-31 14:48:11 +0800

    test

--- app/WeChat/Response.php
+++ app/WeChat/Response.php
@@ -369,7 +369,7 @@ class Response
                          $pic_url="http://weix2.hengdianworld.com" . $result->picurl;
                      }*/
 
-                $pic_url = "https://test.hdymxy.com/" . $result->picurl;
+                $pic_url = "https://test-control.hdymxy.com/" . $result->picurl;
 
                 /*索引图检查结束*/
                 $new = new News();
