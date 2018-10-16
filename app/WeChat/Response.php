26782363cdd8e6e9064bc187082a69b835c0871d
wangke168 <wangke168@gmail.com>
2018-10-16 14:46:57 +0800

    test

--- app/WeChat/Response.php
+++ app/WeChat/Response.php
@@ -316,25 +316,25 @@ class Response
         switch ($type) {
             case 1:
                 $row = WechatArticle::focusPublished($eventkey)
-                    ->skip(0)->take(8)->get();
+                    ->skip(0)->take(1)->get();
                 break;
             case 2:
                 $row = WechatArticle::where('classid', $menuid)
                     ->usagePublished($eventkey)
-                    ->skip(0)->take(8)->get();
+                    ->skip(0)->take(1)->get();
                 break;
             case 3:
                 $keyword = $this->check_keywowrd($keyword);
                 $row = WechatArticle::whereRaw('FIND_IN_SET("' . $keyword . '", keyword)')
                     ->usagePublished($eventkey)
-                    ->skip(0)->take(8)->get();
+                    ->skip(0)->take(1)->get();
                 break;
         }
         if ($row) {
             $content = array();
-            foreach ($row as $result) {
-                $url = $result->url;
-                $id = $result->id;
+//            foreach ($row as $result) {
+                $url = $row->url;
+                $id = $row->id;
                 /*如果只直接跳转链接页面时，判断是否已经带参数*/
                 if ($url != '') {
                     /*链接跳转的数据统计*/
@@ -369,18 +369,18 @@ class Response
                          $pic_url="http://weix2.hengdianworld.com" . $result->picurl;
                      }*/
 
-                $pic_url = "https://wx-control.hdyuanmingxinyuan.com/" . $result->picurl;
+                $pic_url = "https://wx-control.hdyuanmingxinyuan.com/" . $row->picurl;
 
                 /*索引图检查结束*/
                 $new = new News();
-                $new->title = $result->title;
-                $new->description = $result->description;
+                $new->title = $row->title;
+                $new->description = $row->description;
                 $new->url = $url;
 //                $new->image = "http://weix2.hengdianworld.com/" . $result->picurl;
                 $new->image = $pic_url;
-                $content[] = $new;
-            }
-            $this->app->staff->message($content)->by('1001@u_hengdian')->to($openid)->send();
+//                $content[] = $new;
+//            }
+            $this->app->staff->message($new)->by('1001@u_hengdian')->to($openid)->send();
         }
 
     }
