0458949ec356d93da558b929b43b2666f3aaadad
wangke168 <wangke168@gmail.com>
2018-10-16 14:20:12 +0800

    test

--- app/WeChat/Response.php
+++ app/WeChat/Response.php
@@ -93,9 +93,8 @@ class Response
     public function click_request($openid, $menuid)
     {
         $eventkey = $this->usage->get_openid_info($openid)->eventkey;
-        $this->add_menu_click_hit($openid, $menuid); //增加点击数统计
         $this->request_news($openid, $eventkey, '2', '', $menuid);
-
+        $this->add_menu_click_hit($openid, $menuid); //增加点击数统计
 //        return $content;
     }
 
@@ -382,7 +381,6 @@ class Response
                 $content[] = $new;
             }
             $this->app->staff->message($content)->by('1001@u_hengdian')->to($openid)->send();
-//            return [$content];
         }
 
     }
