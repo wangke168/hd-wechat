f69c9f12b3c5892ebef08a43a172f1c5bd4ade39
wangke168 <wangke168@gmail.com>
2019-06-13 19:19:48 +0800

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -110,7 +110,7 @@ Route::get('/test/updateclickinfo','TestController@update_click_info');
 Route::get('/test/ordersend/{id}/{openid?}','TestController@order_send');
 Route::get('/test/orderconfrim/{id}/{openid?}','TestController@order_confrim');
 
-Route::get('/test/test','Test\TestController@uploadimg');
+Route::get('/test/test','Test\TestController@tag');
 
 Route::get('/test/article/detail', 'Test\TestController@detail_test');
 
