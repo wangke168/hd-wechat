35c09ce851f5d2ee7e404d48450a5091dd34c3f8
wangke168 <wangke168@gmail.com>
2019-06-13 16:51:32 +0800

    test

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -56,11 +56,11 @@ class TestController extends Controller
                 break;
             case 'add':
 //                $response->make_user_tag($openid,$eventkey); //标签管理
+                $response->make_user_tag($openid, $eventkey);
 
-
-                if ($this->usage->query_tag_id($eventkey)) { //获取eventkey对应的tag
-                    $this->app->user_tag->batchTagUsers([$openid], $this->usage->query_tag_id($eventkey)); //增加标签
-                }
+//                if ($this->usage->query_tag_id($eventkey)) { //获取eventkey对应的tag
+//                    $this->app->user_tag->batchTagUsers([$openid], $this->usage->query_tag_id($eventkey)); //增加标签
+//                }
 //                return $this->app->user_tag->batchTagUsers([$openid], '100'); //增加标签
 //                return $this->app->user_tag->batchTagUsers([$openid], '100');
                 break;
