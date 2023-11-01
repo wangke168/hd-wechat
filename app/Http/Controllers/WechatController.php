d287bb3b6be60a74e9f89de95a620a5e1efae08a
wangke168 <wangke168@gmail.com>
2023-11-01 06:33:58 +0000

    微调

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -18,16 +18,16 @@ class WechatController extends Controller
     public function serve()
     {
 
-        \Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+//        \Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $wechat = app('wechat');
         $userService = $wechat->user;
-        \Log::info('1');
+//        \Log::info('1');
 
         $wechat->server->setMessageHandler(function ($message) use ($userService) {
-            \Log::info('2');
+//            \Log::info('2');
             $openid = $userService->get($message->FromUserName)->openid;
-          
+
             $eventkey = $message->EventKey;
             $response = new Response();
             switch ($message->MsgType) {
@@ -35,27 +35,15 @@ class WechatController extends Controller
                     # 事件消息...
                     switch ($message->Event) {
                         case 'CLICK':
-                            switch ($message->EventKey) {
-                                case "100":
-                                    $content = new Text();
-                                    $content->content = "官方客服电话" . "\n" . "4009057977";
-                                    return $content;
-                                case "200":
-                                    $content = new Text();
-                                    $content->content = "米友圈专享特惠横店影视城门票将于后续上线，敬请期待。";
-                                    return $content;
-                                default:
-                                    $response->click_request($openid, $message->EventKey);
-                                    break;
-                            }
+                            $response->click_request($openid, $message->EventKey);
                             break;
                         case 'subscribe':
                             #关注事件
                             $eventkey = $message->EventKey;
                             //检查是不是酒店会员中心的分销二维码，如果是，转成1007
-                          /*  if (strlen($eventkey) >= 15) {
-                                $eventkey = "qrscene_1007";
-                            }*/
+                            /*  if (strlen($eventkey) >= 15) {
+                                  $eventkey = "qrscene_1007";
+                              }*/
                             if (substr($eventkey, 0, 7) == 'qrscene') {
                                 $eventkey = substr($eventkey, 8);
                             } else {
@@ -74,28 +62,28 @@ class WechatController extends Controller
                             #重复关注事件
                             $eventkey = $message->EventKey;
                             //检查是不是酒店会员中心的分销二维码，如果是，转成1007
-                           /* if (strlen($eventkey) >= 15) {
-                                $eventkey = "1007";
-                            }*/
+                            /* if (strlen($eventkey) >= 15) {
+                                 $eventkey = "1007";
+                             }*/
                             $response->insert_subscribe($openid, $eventkey, 'scan'); //更新openid信息
                             $response->make_user_tag($openid, $eventkey); //标签管理
                             $response->request_focus($openid, $eventkey); //推送关注信息
-                           /* if ($eventkey == "1336") {
-                                $tour = new Tour();
-                                $content = new Text();
-                                $content->content = $tour->verification_subscribe($openid, '1');
-                                return $content;
+                            /* if ($eventkey == "1336") {
+                                 $tour = new Tour();
+                                 $content = new Text();
+                                 $content->content = $tour->verification_subscribe($openid, '1');
+                                 return $content;
 
-                            } else {
-                                $response->insert_subscribe($openid, $eventkey, 'scan'); //更新openid信息
-                                $response->make_user_tag($openid, $eventkey); //标签管理
-                                $response->request_focus($openid, $eventkey); //推送关注信息
-
-
-                                //      $response->request_focus_temp($openid, $eventkey); //黄金周景区预定推送
+                             } else {
+                                 $response->insert_subscribe($openid, $eventkey, 'scan'); //更新openid信息
+                                 $response->make_user_tag($openid, $eventkey); //标签管理
+                                 $response->request_focus($openid, $eventkey); //推送关注信息
 
 
-                            }*/
+                                 //      $response->request_focus_temp($openid, $eventkey); //黄金周景区预定推送
+
+
+                             }*/
                             break;
                         case 'unsubscribe':
                             #取消关注事件
