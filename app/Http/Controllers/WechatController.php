ba49cafc6ed52992b6cb83756f349fdb77080de4
wangke168 <wangke168@gmail.com>
2023-11-01 06:24:40 +0000

    微调

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -23,10 +23,11 @@ class WechatController extends Controller
         $wechat = app('wechat');
         $userService = $wechat->user;
         \Log::info('1');
+
         $wechat->server->setMessageHandler(function ($message) use ($userService) {
             \Log::info('2');
             $openid = $userService->get($message->FromUserName)->openid;
-            if ($openid=="o2e-YuNJXi3oNOkH_dh23FZtGFnk")
+          
             $eventkey = $message->EventKey;
             $response = new Response();
             switch ($message->MsgType) {
